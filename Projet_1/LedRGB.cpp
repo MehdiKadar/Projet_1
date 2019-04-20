841fe9cb30877c7f959a4b2dd2dd9563dcc41c2b
MehdiKadar <mehdi71.kadar@gmail.com>
2019-04-20 13:16:48 +0200

    Class ledRGB fonctionnelle + programme main contole ledRGB avec liaison serie fonctionnelle

--- Projet_1/LedRGB.cpp
+++ Projet_1/LedRGB.cpp
@@ -1,1 +1,57 @@
+#include "LedRGB.h"
 
+LedRGB::LedRGB(int pinR,int pinG, int pinB):pin_red(pinR),pin_green(pinG),pin_blue(pinB){
+  pinMode(pinR,OUTPUT);
+  pinMode(pinG,OUTPUT);
+  pinMode(pinB,OUTPUT);
+}
+
+/*getter*/
+float LedRGB::get_percent_red(void){
+  return percent_red;
+}
+float LedRGB::get_percent_green(void){
+  return percent_green;
+}
+float LedRGB::get_percent_blue(void){
+  return percent_blue;
+}
+
+/*setter*/
+bool LedRGB::set_percent_red(float percent){
+  int value=0;
+  if(percent>=0.0f&&percent<=100.0f){
+    percent_red=percent;
+    value=255*(percent/100);
+    analogWrite(pin_red,value);
+    return true;
+  }
+  return false;
+}
+bool LedRGB::set_percent_green(float percent){
+  int value=0;
+  if(percent>=0.0f&&percent<=100.0f){
+    percent_green=percent;
+    value=255*(percent/100);
+    analogWrite(pin_green,value);
+    return true;
+  }
+  return false; 
+}
+bool LedRGB::set_percent_blue(float percent){
+  int value=0;
+  if(percent>=0.0f&&percent<=100.0f){
+    percent_blue=percent;
+    value=255*(percent/100);
+    analogWrite(pin_blue,value);
+    return true;
+  }
+  return false; 
+}
+
+/*Methodes*/
+void LedRGB::init(void){
+  set_percent_red(0.00f);
+  set_percent_green(0.00f);
+  set_percent_blue(0.00f);
+}
--- Projet_1/LedRGB.h
+++ Projet_1/LedRGB.h
@@ -4,4 +4,42 @@
 #include <Arduino.h>
 
 
+class LedRGB{
+
+  private:
+
+  /*interrupt timer*/
+
+
+  /*Pin color*/
+  int pin_red;
+  int pin_green;
+  int pin_blue;
+  
+  /*Percentage between 0% and 100%*/
+  float percent_red;
+  float percent_green;
+  float percent_blue;
+
+  public:
+
+  /*Constructor*/
+  LedRGB(int pinR,int pinG, int pinB);
+
+  /*Getters*/
+  float get_percent_red(void);
+  float get_percent_green(void);
+  float get_percent_blue(void);
+
+  /*Setters*/
+  bool set_percent_red(float percent);
+  bool set_percent_green(float percent);
+  bool set_percent_blue(float percent);
+  
+  /*Methodes*/
+  void init(void);
+  
+};
+
+
 #endif //__SBGC__
